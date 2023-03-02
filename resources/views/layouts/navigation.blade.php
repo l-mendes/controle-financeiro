37545c1d0696ffbe0b9c6a455524f1b014e07e4a
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2023-03-01 21:00:09 -0300

    Add link to navigation header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,10 @@
 <nav class="hidden md:block min-h-screen md:w-[260px] overflow-y-auto bg-violet-900 text-gray-100 py-3 px-4">
     <header class="flex flex-1 justify-center border-b border-b-gray-500 py-3">
-        <h1 class="sm:text-lg md:text-xl font-medium">Controle Financeiro</h1>
+        <h1 class="sm:text-lg md:text-xl font-medium">
+            <a href="{{route('dashboard')}}">
+                Controle Financeiro
+            </a>
+        </h1>
     </header>
 
     <div class="mt-5 flex flex-col gap-1">
