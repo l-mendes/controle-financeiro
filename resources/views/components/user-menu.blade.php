413c0f4ed8083804c7e242ec40f515c5a5c143da
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2023-03-04 02:12:51 -0300

    feat: User menu fix

--- resources/views/components/user-menu.blade.php
+++ resources/views/components/user-menu.blade.php
@@ -2,10 +2,10 @@
 
 @php
 
-$dropdownClasses = 'mt-[-90px] ml-[160px]';
+$dropdownClasses = 'mt-[-140px]';
 
 if ($isMobile) {
-$dropdownClasses = 'mt-[-100px] mr-[-160px]';
+$dropdownClasses = 'mt-[-120px]';
 }
 @endphp
 
@@ -22,7 +22,7 @@ $dropdownClasses = 'mt-[-100px] mr-[-160px]';
         </button>
     </div>
 
-    <div x-show="open" @click="open = false" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="fixed z-100 {{ $dropdownClasses }} w-36 rounded-md shadow-lg origin-top" style="display: none;">
+    <div x-show="open" @click="open = false" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute z-100 {{ $dropdownClasses }} w-full rounded-md shadow-lg origin-top" style="display: none;">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
             <a href="{{ route('profile.edit') }}" class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
                 Perfil
