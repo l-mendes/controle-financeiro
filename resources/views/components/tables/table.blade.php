784f103713225f053b22b875e3a301c7862db994
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2023-03-05 01:14:38 -0300

    faet: Table component

---
+++ resources/views/components/tables/table.blade.php
@@ -0,0 +1,12 @@
+<div class="relative overflow-x-auto shadow-md rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500">
+        <thead class="text-sm text-white uppercase bg-primary whitespace-nowrap">
+            <tr>
+                {{ $thead }}
+            </tr>
+        </thead>
+        <tbody>
+            {{ $tbody }}
+        </tbody>
+    </table>
+</div>
