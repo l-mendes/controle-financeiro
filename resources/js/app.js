f0cb260e0f7a160ebed939cb2c4339674d3b757b
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2023-03-12 23:39:12 -0300

    feat: Add input mask lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
-
 import toastr from 'toastr';
+import Inputmask from 'inputmask';
 
 window.Alpine = Alpine;
 
 window.toastr = toastr;
+
+window.Inputmask = Inputmask;
 
 Alpine.start();
