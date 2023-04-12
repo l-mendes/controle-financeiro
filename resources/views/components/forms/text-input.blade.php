70d1d9f0bb27592b088e767972924963f26d6b96
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2023-04-12 02:08:03 +0000

    bugfix: Fix text input name property

--- resources/views/components/forms/text-input.blade.php
+++ resources/views/components/forms/text-input.blade.php
@@ -8,6 +8,6 @@
     }
 @endphp
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+<input {{ $disabled ? 'disabled' : '' }} name="{{ $name }}" {!! $attributes->merge([
     'class' => "border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm $classes",
 ]) !!}>
